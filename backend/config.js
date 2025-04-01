76b6d7d107ced489814cf381ff4eba3e8875a115
danray472 <danray472@gmail.com>
2025-04-02 01:46:15 +0300

    changes

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,4 @@
-export const port = process.env.PORT || 5555; // Use Render's dynamic port, or fallback to 5555 for local development
+// config.js
 
+export const PORT = process.env.PORT || 5555; // Use Render's dynamic port or fallback to 5555 for local development
 export const mongoDBURL = "mongodb+srv://danray472:dseProject@dseproject.csx9lbj.mongodb.net/?retryWrites=true&w=majority&appName=DseProject";
-
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
