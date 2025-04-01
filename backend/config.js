1f3ebac0f5301857a1fea957d7996651f11101d5
danray472 <danray472@gmail.com>
2025-04-01 21:19:38 +0300

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://danray472:dseProject@dseproject.csx9lbj.mongodb.net/?retryWrites=true&w=majority&appName=DseProject";
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
