949b9129ba53ff66a1efa8599117b010fe347b60
danray472 <danray472@gmail.com>
2025-04-02 15:35:41 +0300

    chnages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure the correct base path for deployment
+  base: '/frontend', // Ensure the correct base path for deployment
 })
