4fa899db59b21e9cb51fa151e845aee9545470d4
danray472 <danray472@gmail.com>
2025-04-02 16:32:08 +0300

    changes

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,6 @@ import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
 import booksRoute from './routes/booksRoute.js';
 import cors from 'cors';
-import dotenv from 'dotenv';
-
-// Load environment variables from .env file
-dotenv.config();
 
 const app = express();
 
@@ -14,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 // Middleware for handling CORS POLICY
-// Allow the frontend domain to access the backend API
+// Option 1: Allow All Origins with Default of cors(*)
 app.use(
   cors({
     origin: 'https://mern-bookstoreproj-1.onrender.com', // Update this to the correct frontend URL
@@ -30,9 +26,8 @@ app.get('/', (request, response) => {
 
 app.use('/books', booksRoute);
 
-// Connect to MongoDB and start the server
 mongoose
-  .connect(mongoDBURL)
+  .connect(mongoDBURL) 
   .then(() => {
     console.log('Server connected to database');
     app.listen(PORT, () => {
